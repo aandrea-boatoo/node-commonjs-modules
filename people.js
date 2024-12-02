f5e5687e54c814d7ddbddd74a0fcdc036733fd67
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2024-12-02 09:55:32 +0100

    Link full name

--- people.js
+++ people.js
@@ -1,7 +1,14 @@
 const { fullName } = require("./names");
 const { hobbies } = require("./hobbies");
+fullName("Andrea", "Boato");
+hobbies("Rugby", "Calcio", "Piano",)
+const separateName = fullName("Andrea", "Boato");
+const completeName = separateName.firstName + ' ' + separateName.lastName
 function people() {
-    return { fullname: fullName("Andrea", "Boato"), hobbies: hobbies("Rugby", "Calcio", "Piano") }
+    return {
+        fullName: completeName,
+        hobbies: hobbies("Rugby", "Calcio", "Piano"),
+    }
 }
 
 module.exports = {
